648c0ba00ec33d16ab833e6aa8474aff552cd755
raven1zed <raven1zed007@gmail.com>
2025-12-30 02:00:30 +0700

    feat: Introduce openSEF CMake build system and update flake to unstable for wlroots support.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "VitusOS Ares - openSEF Development Environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # Using unstable for wlroots availability
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
